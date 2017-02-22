473ac20b4e1e72bbef27d725bfa389b30dea37f4
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-02-22 11:59:45 +0900

    libzmq 4.0.5に対応。

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@
 // You should have received a copy of the GNU General Public License
 // along with dromozoa-zmq.  If not, see <http://www.gnu.org/licenses/>.
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <errno.h>
 #include <stdint.h>
 
@@ -22,13 +26,14 @@
 
 #include "common.hpp"
 
+#ifdef HAVE_ZMQ_CURVE_KEYPAIR
+extern "C" {
+  int zmq_curve_keypair (char*, char*);
+}
+#endif
+
 namespace dromozoa {
   namespace {
-    void impl_has(lua_State* L) {
-      const char* capability = luaL_checkstring(L, 1);
-      luaX_push(L, zmq_has(capability));
-    }
-
     void impl_version(lua_State* L) {
       int major = 0;
       int minor = 0;
@@ -38,6 +43,13 @@ namespace dromozoa {
       luaX_push(L, minor);
       luaX_push(L, patch);
     }
+
+#ifdef HAVE_ZMQ_HAS
+    void impl_has(lua_State* L) {
+      const char* capability = luaL_checkstring(L, 1);
+      luaX_push(L, zmq_has(capability));
+    }
+#endif
 
     void impl_poll(lua_State* L) {
       luaL_checkany(L, 1);
@@ -92,6 +104,7 @@ namespace dromozoa {
       }
     }
 
+#ifdef HAVE_ZMQ_CURVE_KEYPAIR
     void impl_curve_keypair(lua_State* L) {
       std::vector<char> z85_public_key(41);
       std::vector<char> z85_secret_key(41);
@@ -102,7 +115,9 @@ namespace dromozoa {
         lua_pushlstring(L, &z85_secret_key[0], 40);
       }
     }
+#endif
 
+#ifdef HAVE_ZMQ_CURVE_PUBLIC
     void impl_curve_public(lua_State* L) {
       size_t size = 0;
       const char* z85_secret_key = luaL_checklstring(L, 1, &size);
@@ -119,13 +134,14 @@ namespace dromozoa {
         push_error(L);
       }
     }
+#endif
 
     void impl_z85_decode(lua_State* L) {
       size_t size = 0;
       const char* data = luaL_checklstring(L, 1, &size);
       if (size % 5 == 0) {
         std::vector<char> buffer(size * 4 / 5);
-        if (zmq_z85_decode(reinterpret_cast<uint8_t*>(&buffer[0]), data)) {
+        if (zmq_z85_decode(reinterpret_cast<uint8_t*>(&buffer[0]), const_cast<char*>(data))) {
           lua_pushlstring(L, &buffer[0], buffer.size());
         } else {
           push_error(L);
@@ -141,7 +157,7 @@ namespace dromozoa {
       const char* data = luaL_checklstring(L, 1, &size);
       if (size % 4 == 0) {
         std::vector<char> buffer(size * 5 / 4 + 1);
-        if (zmq_z85_encode(&buffer[0], reinterpret_cast<const uint8_t*>(data), size)) {
+        if (zmq_z85_encode(&buffer[0], reinterpret_cast<uint8_t*>(const_cast<char*>(data)), size)) {
           luaX_push(L, &buffer[0]);
         } else {
           push_error(L);
@@ -154,12 +170,18 @@ namespace dromozoa {
   }
 
   void initialize_main(lua_State* L) {
-    luaX_set_field(L, -1, "has", impl_has);
     luaX_set_field(L, -1, "version", impl_version);
+#ifdef HAVE_ZMQ_HAS
+    luaX_set_field(L, -1, "has", impl_has);
+#endif
     luaX_set_field(L, -1, "poll", impl_poll);
     luaX_set_field(L, -1, "proxy", impl_proxy);
+#ifdef HAVE_ZMQ_CURVE_KEYPAIR
     luaX_set_field(L, -1, "curve_keypair", impl_curve_keypair);
+#endif
+#ifdef HAVE_ZMQ_CURVE_PUBLIC
     luaX_set_field(L, -1, "curve_public", impl_curve_public);
+#endif
     luaX_set_field(L, -1, "z85_decode", impl_z85_decode);
     luaX_set_field(L, -1, "z85_encode", impl_z85_encode);
   }
